e306d1bc8339bc0128832ab9b46093400a2dd3f3
genadi53 <kotka5353@gmail.com>
2022-02-12 14:21:28 +0200

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState<string[]>([]);
+
+  const inputHandler = (text: string) => {
+    setTodo(text);
+  };
+
+  const addTodoHandler = () => {
+    setTodos((currentTodos) => [...currentTodos, todo]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={{ padding: 30 }}>
       <StatusBar style="auto" />
+      {
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Enter something..."
+            style={{
+              borderBottomColor: "black",
+              borderBottomWidth: 1,
+              padding: 15,
+            }}
+            value={todo}
+            onChangeText={inputHandler}
+          />
+          <Button
+            title="ADD"
+            onPress={() => {
+              addTodoHandler();
+            }}
+          />
+        </View>
+      }
+      <View>
+        {todos.map((td: string, idx) => (
+          <Text key={idx}>{td}</Text>
+        ))}
+      </View>
     </View>
   );
 }
@@ -13,8 +49,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
 });
